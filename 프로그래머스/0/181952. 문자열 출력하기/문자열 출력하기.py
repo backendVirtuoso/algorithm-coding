3c1cd327b36f37c4085fcf9dcde12a51bfaf0fbc
Hwang JunHa <129765568+backendVirtuoso@users.noreply.github.com>
2024-04-07 12:22:14 +0900

    [level 0] Title: 문자열 출력하기, Time: 18.44 ms, Memory: 7.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.py
@@ -0,0 +1,8 @@
+str = input()
+
+if 1 <= len(str) and len(str) <= 1000000:
+        str.replace(" ", "")
+        print(str)
+        
+#if 1 <= len(str) and len(str) <= 1000000 and str != " ":
+#    print(str)
