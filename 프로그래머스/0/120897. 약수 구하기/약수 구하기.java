837c1612a2bb79433711506f3b2654a9285bca8d
Hwang JunHa <129765568+backendVirtuoso@users.noreply.github.com>
2024-04-07 16:53:39 +0900

    [level 0] Title: 약수 구하기, Time: 0.52 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int n) {
         int a = 0;
@@ -6,6 +8,7 @@ class Solution {
 				a++;
 			}
 		}
+        
 		int [] answer = new int[a];
 		int k = 0;
 		for (int i = 1; i <= n; i++) {
@@ -15,15 +18,8 @@ class Solution {
 			}
 		}
 
-        for (int i = 1; i < answer.length; i++) {
-			for (int j = 0; j < i; j++) {
-				if (answer[i] < answer[j]) {
-					int temp = answer[i];
-					answer[i] = answer[j];
-					answer[j] = temp;
-				}
-			}
-		}
+        Arrays.sort(answer);
+        
         return answer;
     }
 }
