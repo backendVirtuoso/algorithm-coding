a718c73c79c4876169f9c867baf25937d3ba8c72
Hwang JunHa <129765568+backendVirtuoso@users.noreply.github.com>
2024-07-13 22:49:50 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -4,5 +4,8 @@ class Solution {
         answer[0] = money / 5500;
         answer[1] = money % 5500;
         return answer;
+        
+        // 다른 사람의 풀이
+        // return new int[] { money / 5500, money % 5500 };
     }
 }
