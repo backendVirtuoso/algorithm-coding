1951981fc9a77cf3ada36712485109b8802de9e5
Hwang JunHa <129765568+backendVirtuoso@users.noreply.github.com>
2024-08-19 18:49:54 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean solution(String s) {
-        //return s.matches("[0-9]{4}|[0-9]{6}");
+        // return s.matches("[0-9]{4}|[0-9]{6}");
         
         // 라이브러리
         if (s.length() != 4 && s.length() != 6) return false;
