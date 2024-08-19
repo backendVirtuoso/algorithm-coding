695d37912331dcbd791515db82b4b4b3d38ac6c5
Hwang JunHa <129765568+backendVirtuoso@users.noreply.github.com>
2024-08-19 19:59:01 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.24 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/README.md
+++ 프로그래머스/0/120888. 중복된 문자 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중복된 문자 제거 - 120888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888) 
 
 ### 성능 요약
 
-메모리: 86.6 MB, 시간: 14.11 ms
+메모리: 76.5 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 07일 17:17:53
+2024년 08월 19일 19:59:00
 
 ### 문제 설명
 
--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,5 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public String solution(String my_string) {
+        Set<Character> used = new HashSet<>();
+
+        StringBuilder builder = new StringBuilder();
+        for (char c : my_string.toCharArray()) {
+            if (used.contains(c)) continue;
+            used.add(c);
+            builder.append(c);
+        }
+
+        return builder.toString();
+        /*
         String result = "";
 		
 		for (int i = 0; i < my_string.length(); i++) {
@@ -7,5 +21,6 @@ class Solution {
 				result += my_string.charAt(i);
 		}
 		return result;
+        */
     }
 }
