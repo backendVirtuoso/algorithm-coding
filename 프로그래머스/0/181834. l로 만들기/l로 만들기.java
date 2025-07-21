3e16279b617edcbdb10b06b871ce36b51c09ebfe
Hwang JunHa <129765568+backendVirtuoso@users.noreply.github.com>
2025-07-22 00:29:07 +0900

    [level 0] Title: l로 만들기, Time: 11.04 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : myString.toCharArray()) {
+            sb.append(c < 'l' ? 'l' : c);
+        }
+        return sb.toString();
+    }
+}
