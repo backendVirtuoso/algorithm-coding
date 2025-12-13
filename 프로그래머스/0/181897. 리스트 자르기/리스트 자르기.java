3f65666998ddeb8d5c0a1432b7bd3bf7caea5645
Hwang JunHa <129765568+backendVirtuoso@users.noreply.github.com>
2025-12-13 23:27:07 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,17 +1,30 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, int[] s, int[] arr) {
         int a = s[0], b = s[1], c = s[2];
-        List<Integer> res = new ArrayList<>();
+        int len, idx = 0;
 
         switch (n) {
-            case 1 -> { for (int i = 0; i <= b; i++) res.add(arr[i]); }
-            case 2 -> { for (int i = a; i < arr.length; i++) res.add(arr[i]); }
-            case 3 -> { for (int i = a; i <= b; i++) res.add(arr[i]); }
-            case 4 -> { for (int i = a; i <= b; i += c) res.add(arr[i]); }
+            case 1: len = b + 1; break;
+            case 2: len = arr.length - a; break;
+            case 3: len = b - a + 1; break;
+            default: len = (b - a) / c + 1;
         }
 
-        return res.stream().mapToInt(i -> i).toArray();
+        int[] res = new int[len];
+
+        switch (n) {
+            case 1:
+                for (int i = 0; i <= b; i++) res[idx++] = arr[i];
+                break;
+            case 2:
+                for (int i = a; i < arr.length; i++) res[idx++] = arr[i];
+                break;
+            case 3:
+                for (int i = a; i <= b; i++) res[idx++] = arr[i];
+                break;
+            case 4:
+                for (int i = a; i <= b; i += c) res[idx++] = arr[i];
+        }
+        return res;
     }
 }
